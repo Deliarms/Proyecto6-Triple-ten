1ef5b4c1865f833bace262270a47fe9360be013f
Delia_rms <delia1704@gmail.com>
2024-10-11 15:33:19 -0400

    actualizacion

--- app.py
+++ app.py
@@ -9,8 +9,20 @@ hist_boton = st.button('Contruir histograma')
 if hist_boton:
     st.write('Creacion de histograma de los valores del odometro de los autos')
     fig = px.histogram(data, x='odometer')
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
     
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') 
+fig = px.scatter(car_data, x="odometer", y="price") 
+fig.show() 
+
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación  esta funcional.')
 
 
  
